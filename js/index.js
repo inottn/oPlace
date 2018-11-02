b71473f752ba417c3493fc30580825a5d5574d86
inottn <inottn@outlook.com>
2018-11-02 11:36:26 +0800

    add: 完成home元素基本布局

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const places = document.querySelectorAll('.place')
+
+Array.from(places).forEach((place, index, places) => {
+  place.addEventListener('click', function(e) {
+    places.forEach((p, i) => {
+      if (p === this) return
+      if (i < index) p.classList.add('hide-left')
+      else p.classList.add('hide-right')
+    })
+  })
+})
