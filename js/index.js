03cf0a77a3473ee48c8d253bd7665410f48b8326
inottn <inottn@outlook.com>
2018-11-03 21:11:43 +0800

    refactor: 使用img标签替代place元素的background-image样式，使用margin-left替代transform，使动画更平滑

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const places = document.querySelectorAll('.place')
 
 Array.from(places).forEach((place, index, places) => {
   place.addEventListener('click', function(e) {
+    this.classList.add('click')
     places.forEach((p, i) => {
       if (p === this) return
       if (i < index) p.classList.add('hide-left')
