8567a969a2dbe9007c43c3c3158be9ad180bf066
inottn <inottn@outlook.com>
2018-11-04 15:25:13 +0800

    add: 完成place-introduction元素基本布局

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 const places = document.querySelectorAll('.place')
+const placeIntroductions = document.querySelectorAll('.place-introduction')
 
 Array.from(places).forEach((place, index, places) => {
   place.addEventListener('click', function(e) {
+    this.addEventListener('transitionend', function(e) {
+      if (e.target !== this || e.pseudoElement !== "") return
+      placeIntroductions[index].classList.add('show')
+    })
     this.classList.add('click')
     places.forEach((p, i) => {
       if (p === this) return
