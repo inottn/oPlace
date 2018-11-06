5f4b35b8449b9f48d6ef8b34d5fe4b97b9d94f41
inottn <inottn@outlook.com>
2018-11-06 11:45:16 +0800

    add: 完成了place-introduction-close-btn元素的布局及其点击事件

--- js/index.js
+++ js/index.js
@@ -1,17 +1,30 @@
 const places = document.querySelectorAll('.place')
 const placeIntroductions = document.querySelectorAll('.place-introduction')
+const closePlaceIntroductionBtns = document.querySelectorAll('.place-introduction-close-btn')
 
 Array.from(places).forEach((place, index, places) => {
   place.addEventListener('click', function(e) {
-    this.addEventListener('transitionend', function(e) {
-      if (e.target !== this || e.pseudoElement !== "") return
-      placeIntroductions[index].classList.add('show')
-    })
+    if (e.target === closePlaceIntroductionBtns[index] || place.classList.contains('click')) return
+
     this.classList.add('click')
+
     places.forEach((p, i) => {
       if (p === this) return
       if (i < index) p.classList.add('hide-left')
-      else p.classList.add('hide-right')
+    })
+
+    placeIntroductions[index].classList.add('show')
+    console.log(1)
+  })
+})
+
+Array.from(closePlaceIntroductionBtns).forEach((btn, index, btns) => {
+  btn.addEventListener('click', function(e) {
+    placeIntroductions[index].classList.remove('show')
+    places[index].classList.remove('click')
+    places.forEach((p, i) => {
+      if (p === this) return
+      if (i < index) p.classList.remove('hide-left')
     })
   })
 })
